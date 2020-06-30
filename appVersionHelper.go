ad9a8f5769dbbed3a29fc2803c5c905920b089db
StefMa <StefMaDev@outlook.com>
2020-06-30 20:32:38 +0200

    Extract getAppVersions

--- appVersionHelper.go
+++ appVersionHelper.go
@@ -3,9 +3,8 @@ package handler
 import (
 	"fmt"
 	"net/http"
-	"log"
 	"strings"
-	"github.com/PuerkitoBio/goquery"
+	"stefma.guru/appVersions/usecase"
 )
 
 func HandleFunc(w http.ResponseWriter, r *http.Request) {
@@ -14,62 +13,21 @@ func HandleFunc(w http.ResponseWriter, r *http.Request) {
 
 	if androidQuery != "" {
 		androidAppIds := strings.Split(androidQuery, ",")
-		for _, androidId := range androidAppIds {
-			android(w, androidId)
+		appVersions := usecase.AndroidVersions(androidAppIds)
+		for _, appVersion := range appVersions {
+			fmt.Fprintf(w, "Android - %s: %s\n", appVersion.AppId, appVersion.Version)
 		}
 	}
 
 	if iosQuery != "" {
 		iosAppIds := strings.Split(iosQuery, ",")
-		for _, iosAppId := range iosAppIds {
-			ios(w, iosAppId)
+		appVersions := usecase.IosVersions(iosAppIds)
+		for _, appVersion := range appVersions {
+			fmt.Fprintf(w, "iOS - %s: %s\n", appVersion.AppId, appVersion.Version)
 		}
 	}
 
 	if androidQuery == "" && iosQuery == "" {
 		fmt.Fprintf(w, "Please add a 'ios' or 'android' query to the url")
 	}
-}
-
-func android(w http.ResponseWriter, androidAppId string) {
-	url := "https://play.google.com/store/apps/details?id=" + androidAppId
-	resp, err := http.Get(url)
-	if err != nil {
-		panic(err)
-	}
-	defer resp.Body.Close()
-	if resp.StatusCode != 200 {
-		log.Fatalf("status code error: %d %s", resp.StatusCode, resp.Status)
-	}
-
-	doc, err := goquery.NewDocumentFromReader(resp.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-	doc.Find(".hAyfc .htlgb").Each(func(i int, s *goquery.Selection) {
-		if i == 6 {
-			fmt.Fprintf(w, "Android - %s: %s\n", androidAppId, s.Text())
-		}
-	})
-}
-
-func ios(w http.ResponseWriter, iosAppId string) {
-	url := "https://apps.apple.com/de/app/" + iosAppId
-	resp, err := http.Get(url)
-	if err != nil {
-		panic(err)
-	}
-	defer resp.Body.Close()
-	if resp.StatusCode != 200 {
-		log.Fatalf("status code error: %d %s", resp.StatusCode, resp.Status)
-	}
-
-	doc, err := goquery.NewDocumentFromReader(resp.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-	doc.Find(".whats-new__latest__version").Each(func(i int, s *goquery.Selection) {
-		version := strings.Replace(s.Text(), "Version ", "" , -1)
-		fmt.Fprintf(w, "iOS - %s: %s\n", iosAppId, version)
-	})
 }
