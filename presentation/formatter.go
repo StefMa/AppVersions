6d20c830e6e6459525f62bdf2c3984482a6ec7e8
StefMa <StefMaDev@outlook.com>
2020-07-08 17:01:08 +0200

    Nicer landingpage

--- presentation/formatter.go
+++ presentation/formatter.go
@@ -4,18 +4,6 @@ import (
 	"stefma.guru/appVersions/usecase"
 )
 
-func HelpText(domain string) string {
-  return  `Please add a 'ios' or 'android' query to the url.
-The output can be changed via the 'format' [json, pretty] query parameter.
-
-Examples:
-https://` + domain + `?android=com.ioki.hamburg,com.ioki.wittlich
-https://` + domain + `?android=com.ioki.hamburg&format=json
-https://` + domain + `?ios=ioki-hamburg/id1400408720,ioki-wittlich/id1377071496
-https://` + domain + `?ios=ioki-hamburg/id1400408720&format=json
-https://` + domain + `?android=com.ioki.hamburg,com.ioki.wittlich&ios=ioki-hamburg/id1400408720&format=json`
-}
-
 func FormatOutput(format string, androidApps []usecase.App, iosApps []usecase.App) string {
   switch format {
   case "json":
@@ -25,4 +13,8 @@ func FormatOutput(format string, androidApps []usecase.App, iosApps []usecase.Ap
   default:
     return formatToPretty(androidApps, iosApps)
   }
+}
+
+func Index() string {
+	return index()
 }
