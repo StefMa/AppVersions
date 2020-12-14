64fb572c14de3722f05114a710fa0c3507586f5c
StefMa <StefMaDev@outlook.com>
2020-12-14 10:25:27 +0100

    Show errors with new design

--- presentation/prettyFormatter.go
+++ presentation/prettyFormatter.go
@@ -17,53 +17,32 @@ type App struct {
 	Version string
 	Rating string
 	Url string
+	Error bool
 }
 
 func formatToPretty(androidApps []usecase.App, iosApps []usecase.App) string {
 	tmpl := template.Must(template.ParseGlob("presentation/template/pretty*.html"))
 	androidAppsTmpl := []App{}
 	for _, androidApp := range androidApps {
-		var name string
-		var version string
-		var url string
-		if androidApp.Error {
-			name = "Error width id"
-			version = "Typo? App not in Play Store?!"
-			url = ""
-		} else {
-			name = androidApp.Name
-			version = androidApp.Version
-			url = androidApp.Url
-		}
 		app := App{
 			Id: androidApp.Id,
-			Name: name,
-			Version: version,
+			Name: androidApp.Name,
+			Version: androidApp.Version,
 			Rating: androidApp.Rating,
-			Url: url,
+			Url: androidApp.Url,
+			Error: androidApp.Error,
 		}
 		androidAppsTmpl = append(androidAppsTmpl, app)
 	}
 	iosAppsTmpl := []App{}
 	for _, iosApp := range iosApps {
-		var name string
-		var version string
-		var url string
-		if iosApp.Error {
-			name = "Error width id"
-			version = "Typo? App not in App Store?!"
-			url = ""
-		} else {
-			name = iosApp.Name
-			version = iosApp.Version
-			url = iosApp.Url
-		}
 		app := App{
 			Id: iosApp.Id,
-			Name: name,
-			Version: version,
+			Name: iosApp.Name,
+			Version: iosApp.Version,
 			Rating: iosApp.Rating,
-			Url: url,
+			Url: iosApp.Url,
+			Error: iosApp.Error,
 		}
 		iosAppsTmpl = append(iosAppsTmpl, app)
 	}
