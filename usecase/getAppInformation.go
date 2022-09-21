9765a954629e857979ddb2f86ff614404559e16f
Ravi Shekhar Jethani <ravi-shekhar.jethani@ioki.com>
2022-09-21 23:32:33 +0200

    My proposed enhancements and simplification of async code

--- usecase/getAppInformation.go
+++ usecase/getAppInformation.go
@@ -2,11 +2,12 @@ package usecase
 
 import (
 	"bytes"
-	"github.com/PuerkitoBio/goquery"
 	"io"
 	"log"
 	"net/http"
 	"sort"
+
+	"github.com/PuerkitoBio/goquery"
 )
 
 type AppsInformation struct {
@@ -25,19 +26,61 @@ type App struct {
 }
 
 func GetAppsInformation(androidAppIds []string, iosAppIds []string) AppsInformation {
-	androidAppsChannel := make(chan []App)
-	iosAppsChannel := make(chan []App)
-	go appInformation(androidAppIds, androidAppsChannel, func(appId string) App {
-		return androidAppInfo(appId)
-	})
-	go appInformation(iosAppIds, iosAppsChannel, func(appId string) App {
-		return iosAppInfo(appId)
-	})
-	androidApps, iosApps := <-androidAppsChannel, <-iosAppsChannel
-	return AppsInformation{
-		AndroidApps: androidApps,
-		IosApps:     iosApps,
+	androidAppsChannel := make(chan App)
+	iosAppsChannel := make(chan App)
+
+	// Step 1: We create separate goroutines for fetching each app's info.
+	// PS: There is no limit here and we may create thousands of goroutines in a worst case. Go can handle
+	// this easily :) not sure about the remote api though. Anyways this should be in our control. Also
+	// the goroutines can run for too long and hand the whole process hence we should always have a way to
+	// tell goroutines to stop. All this enhancements can be made later of course.
+
+	// Get information about all android apps concurrently.
+	for _, id := range androidAppIds {
+		log.Printf("\nstarting task for android app id '%s' ", id)
+		go func(i string) {
+			androidAppsChannel <- androidAppInfo(i)
+		}(id)
 	}
+
+	// Get information about all ios apps concurrently.
+	for _, id := range iosAppIds {
+		log.Printf("\nstarting task for ios app id '%s' ", id)
+		go func(i string) {
+			iosAppsChannel <- iosAppInfo(i)
+		}(id)
+	}
+
+	// info is our final object which will eventually contains info about all required android and ios apps.
+	info := AppsInformation{
+		AndroidApps: []App{},
+		IosApps:     []App{},
+	}
+
+	// Step 2: We wait/block until all goroutines are finished. As a part of that we take advantage of
+	// select statement to do 'io muxing' in multiple channels and keep filling the info object
+	// with app info as when they become available from above goroutines.
+	// Stop when we have got info for all required app ids.
+	log.Printf("\nready to receive app information from background tasks...")
+	for {
+		select {
+		case a := <-androidAppsChannel:
+			log.Printf("\nreceived info about android app id '%s': %v", a.Id, a)
+			info.AndroidApps = append(info.AndroidApps, a)
+		case i := <-iosAppsChannel:
+			log.Printf("\nreceived info about ios app id '%s': %v", i.Id, i)
+			info.IosApps = append(info.IosApps, i)
+		}
+		if len(info.AndroidApps) == len(androidAppIds) && len(info.IosApps) == len(iosAppIds) {
+			log.Printf("\nFinished getting info about all required apps")
+			break
+		}
+	}
+
+	sort.SliceStable(info.AndroidApps, func(i, j int) bool { return info.AndroidApps[i].Name < info.AndroidApps[j].Name })
+	sort.SliceStable(info.IosApps, func(i, j int) bool { return info.IosApps[i].Name < info.IosApps[j].Name })
+
+	return info
 }
 
 func fetchWebsite(url string) ([]byte, bool) {
@@ -57,21 +100,6 @@ func fetchWebsite(url string) ([]byte, bool) {
 		return nil, false
 	}
 	return bodyBytes, true
-}
-
-func appInformation(appIds []string, appsChannel chan []App, f func(appId string) App) {
-	apps := []App{}
-	appChannel := make(chan App)
-	for _, appId := range appIds {
-		go func(appId string) {
-			appChannel <- f(appId)
-		}(appId)
-	}
-	for range appIds {
-		apps = append(apps, <-appChannel)
-	}
-	sort.Slice(apps, func(i, j int) bool { return apps[i].Name < apps[j].Name })
-	appsChannel <- apps
 }
 
 func extractInformation(body []byte, htmlClass string, selector func(int, *goquery.Selection) string) (string, bool) {
